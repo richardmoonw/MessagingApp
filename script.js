b91068eb5b42d03f9c562c9ae0036f95f0373b11
Ricardo <ricardo@themoondev.com>
2020-09-22 22:21:43 -0600

    Socket.io implementation already working

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const socket = io('http://localhost:3000');
+const messageForm = document.getElementById('message-form');
+const messageInput = document.getElementById('message');
 
 socket.on('message-chat', (data) => {
     console.log(data);
+});
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
 });
--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(__dirname + '/script.js');
+});
+
+app.get('/bootstrap.css', (req, res) => {
+    res.sendFile(__dirname + '/bootstrap.css');
 })
 
-io.on('connection', socket => {
-    console.log('new user');
-    socket.emit('message-chat', 'Test');
+io.on('connect', socket => {
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message)
+    });
 });
 
 var myapp = app.listen(8080);
