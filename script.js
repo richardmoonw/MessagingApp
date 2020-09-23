4196b4d1ffa9cc2a0ec3e1e7f83b93a8ec0b0c07
Ricardo <ricardo@themoondev.com>
2020-09-23 17:30:53 -0600

    External sockets functionality almost implemented

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const socket = io('http://localhost:3000');
+const socket = io('http://192.168.1.99:3000');
 const messageForm = document.getElementById('message-form');
 const messageInput = document.getElementById('message');
 const messageContainer = document.getElementById('message-container');
+
 
 socket.on('message-chat', (message) => {
     appendMessage(message);
 });
 
+socket.on('external_message', message => {
+    appendMessage(message)
+});
+
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
-    appendMessage(message)
+    appendMessage(message);
     socket.emit('send-chat-message', message);
     messageInput.value = '';
 });
